1e69be267a443cc32218a957e60a26d95a984674
Anibal Ortega <anibalortegap@gmail.com>
2022-06-20 19:18:58 -0500

    New log in console

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[]args){
         System.out.println("Practice of Jenkins");
+        System.out.println("Connecting GitHub - Jenkins");
+
     }
 }
