61897c33a14355be72729e9469468ff48a8c7442
Anibal Ortega <anibalortegap@gmail.com>
2022-06-20 20:30:35 -0500

    Correction typo

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String[]args){
         System.out.println("Practice of Jenkins");
         System.out.println("Connecting GitHub - Jenkins");
-        System.out.println("Created new branch and cofigurate in Jenkins")
+        System.out.println("Created new branch and cofigurate in Jenkins");
     }
 }
